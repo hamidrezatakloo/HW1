1684a9c6a60fb1beeb32c12c5d6f32dad7108043
hamidreza <takloohamidreza@gmail.com>
2021-12-03 03:13:15 -0500

    read text file as rdd, do flatmap for splite each file with space , filter for ignor none alphabt and map eveery word with 1 and reduce key to add same key and save rdd as text in out folder in current path

--- spark.py
+++ spark.py
@@ -1,3 +1,18 @@
-from pyspark import SparkContext
-
-sc = SparkContext("local","justApp")
+from pyspark import SparkContext 
+from operator import add 
+import re 
+ 
+sc = SparkContext("local","justApp")  
+  
+file = sc.textFile("./ShamsDaftar5.txt") 
+ 
+a =( 
+file.flatMap(lambda x: re.split("\s+",x)) 
+ 
+ .filter(lambda x : x and x[0].isalpha()) 
+ 
+ .map(lambda x: (x,1)) 
+ ) 
+b = a.reduceByKey(add) 
+ 
+b.saveAsTextFile("./out") 
